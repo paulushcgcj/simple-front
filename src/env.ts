e639cafc6c4a444c2f2c34b4dc41c83da121500e
Paulo Gomes da Cruz Jr <paulo.cruz.external@bt.com>
2022-12-02 00:32:09 +0000

    feat: adding module support

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,10 @@
 declare global {
-  // eslint-disable-next-line no-unused-vars
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-explicit-any
   interface Window {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     env: any;
   }
 }
 
-// eslint-disable-next-line import/prefer-default-export
+// eslint-disable-next-line import/prefer-default-export, @typescript-eslint/no-explicit-any
 export const env: Record<string, any> = { ...process.env, ...window.env };
